a7d2ae146ae5a2f5da6428e5382c565ea71163dc
Aybars Nazlica <aybarsnazlica@gmail.com>
2023-04-05 23:02:36 +0900

    Rename variables

--- colordomains/classmodule.py
+++ colordomains/classmodule.py
@@ -7,10 +7,10 @@ class BFactorRange:
         return f"{self.num_domains} domains with indices: {self.ranges_list}"
 
     def __create_ranges(self):
-        min, max = 20, 90
-        diff = max - min  # from 20% to 90%
+        min_range, max_range = 20, 90
+        diff = max_range - min_range  # from 20% to 90%
         incr = diff / (self.num_domains - 1)
-        b_fact = list(range(min, max + 1, int(incr)))
+        b_fact = list(range(min_range, max_range + 1, int(incr)))
         return [f"{b}.00" for b in b_fact]
 
     def to_table(self):
